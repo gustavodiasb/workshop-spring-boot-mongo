f81f1530f094b8d0d57a6613752fdda417e98661
gustavodiasb <blasenbauer.gustavo@gmail.com>
2018-09-14 11:46:37 -0300

    Primeiro commit - aula 177

---
+++ src/main/java/com/gustavodiasb/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.gustavodiasb.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gustavodiasb.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ src/main/java/com/gustavodiasb/workshopmongo/resources/UserResource.java
@@ -0,0 +1,28 @@
+package com.gustavodiasb.workshopmongo.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gustavodiasb.workshopmongo.domain.User;
+import com.gustavodiasb.workshopmongo.services.UserService;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+	
+	@Autowired
+	private UserService service;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	// ou @GetMapping
+	public ResponseEntity<List<User>> findAll() {		
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+}
