b3e921fe6ee8a5dfe3eb99c252b24e7cdc5e5c8d
gustavodiasb <blasenbauer.gustavo@gmail.com>
2018-09-14 16:11:09 -0300

    Operacao de instanciacao da base de dados

--- src/main/java/com/gustavodiasb/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gustavodiasb/workshopmongo/resources/UserResource.java
@@ -20,7 +20,15 @@ public class UserResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
 	// ou @GetMapping
-	public ResponseEntity<List<User>> findAll() {		
+	public ResponseEntity<List<User>> findAll() {
+
+		/*
+		User maria = new User("1", "Maria Brown", "maria@gmail.com");
+		User alex = new User("2", "Alex Green", "alex@gmail.com");
+		List<User> list = new ArrayList<>();
+		list.addAll(Arrays.asList(maria,alex));
+		 */
+		
 		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
