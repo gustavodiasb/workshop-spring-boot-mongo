1445f379cc03f77364be6d2f1b67b4cfd470e926
gustavodiasb <blasenbauer.gustavo@gmail.com>
2018-09-15 00:32:08 -0300

    Criando entity post com user aninhado

---
+++ src/main/java/com/gustavodiasb/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.gustavodiasb.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gustavodiasb.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
