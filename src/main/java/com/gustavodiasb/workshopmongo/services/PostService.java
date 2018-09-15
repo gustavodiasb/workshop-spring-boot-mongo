5f96e50f72561b08a25a60de40eed0b41202e62b
gustavodiasb <blasenbauer.gustavo@gmail.com>
2018-09-15 12:52:48 -0300

    obtendo um post por id

---
+++ src/main/java/com/gustavodiasb/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.gustavodiasb.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gustavodiasb.workshopmongo.domain.Post;
+import com.gustavodiasb.workshopmongo.repository.PostRepository;
+import com.gustavodiasb.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
