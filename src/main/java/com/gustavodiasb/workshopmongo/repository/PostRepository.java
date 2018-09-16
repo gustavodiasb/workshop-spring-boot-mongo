1fe3ed7e95c2a58209e250d6bf867f260feb30b5
gustavodiasb <blasenbauer.gustavo@gmail.com>
2018-09-16 19:15:43 -0300

    Consulta com varios criterios

--- src/main/java/com/gustavodiasb/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/gustavodiasb/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.gustavodiasb.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -16,10 +17,18 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	Agora é possível utilizar o nome que desejar com as sintaxes Strings
 	com a personalização abaixo.
 	Um exemplo é a sintaxe 'findByTitle' por 'searchTitle'
-	 */
+	*/
 	@Query("{ 'title': {$regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
 	
+	/*
+	método de pesquisa padronizado pelo Spring
+	findTitleContaining
+	IgnoreCase serve para realizar buscas sem diferenciar letras minúsculas de maiúsculas 
+	*/
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} }, { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 	
 }
--- src/main/java/com/gustavodiasb/workshopmongo/resources/PostResource.java
+++ src/main/java/com/gustavodiasb/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.gustavodiasb.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +34,19 @@ public class PostResource {
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) { // @RequestParam -> deixa a URL com "?"
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@RequestMapping(value="/fullsearch", method=RequestMethod.GET)
+	// ou @GetMapping
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value="text", defaultValue="") String text, // @RequestParam -> deixa a URL com "?"
+			@RequestParam(value="minDate", defaultValue="") String minDate,
+			@RequestParam(value="maxDate", defaultValue="") String maxDate) {			
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L)); // caso surgir problemas para gerar a senha, o Date(0L) gerará a data mínima do Java (01/01/1970)
+		Date max = URL.convertDate(maxDate, new Date()); // new Date() nesse caso gera a data atual do sistema
+		List<Post> list = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(list);
 	}
 	
