b7a45ca11f91b8fcb60aa8e2df529b2e599d1e85
gustavodiasb <blasenbauer.gustavo@gmail.com>
2018-09-16 17:31:54 -0300

    Consulta simples com @query

--- src/main/java/com/gustavodiasb/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/gustavodiasb/workshopmongo/repository/PostRepository.java
@@ -3,13 +3,23 @@ package com.gustavodiasb.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.gustavodiasb.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	
+	
+	/*
+	Agora é possível utilizar o nome que desejar com as sintaxes Strings
+	com a personalização abaixo.
+	Um exemplo é a sintaxe 'findByTitle' por 'searchTitle'
+	 */
+	@Query("{ 'title': {$regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
